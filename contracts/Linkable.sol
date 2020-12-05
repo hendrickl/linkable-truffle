08f38f8b8abcf9ab1c14d2d208cc6e5f26fce799
hendrick <h.lincertin@gmail.com>
2020-12-05 23:30:05 +0100

    updates

--- contracts/Linkable.sol
+++ contracts/Linkable.sol
@@ -1,9 +1,6 @@
-// Project : 0x751afF11CEDa4d298b817DF603d8F91Ce161fEA9
-// Investor : 0x65c2c71FB6b78d07dc1Adc81ecdaC7983A5572D9
-// contract deployed at 0xeA18AC21C095b08FA2b667E19f18171F90BFe180
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.5;
-pragma experimental ABIEncoderV2; 
+pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Linkable {
@@ -29,8 +26,8 @@ contract Linkable {
     address payable public investorAddr;
 
     // fund from investor
-    uint public fund;
-    
+    uint256 public fund;
+
     constructor(address payable projectAddr_) payable {
         investorAddr = msg.sender;
         projectAddr = projectAddr_;
@@ -56,23 +53,25 @@ contract Linkable {
     function getBalance(address addr_) public view returns (uint256) {
         return address(addr_).balance;
     }
-    
+
     // createDemand() creates demand and adds them in the demands array
     function createDemand(
         string memory name_,
         string memory creator_,
         string memory description_,
         string memory location_,
-        uint256 amount_) public onlyProject {
-        DemandFromProject memory demand = DemandFromProject({
-            name: name_,
-            creator: creator_,
-            description: description_,
-            location: location_,
-            amount: amount_,
-            blocked: true,
-            paid: false
-        });
+        uint256 amount_
+    ) public onlyProject {
+        DemandFromProject memory demand =
+            DemandFromProject({
+                name: name_,
+                creator: creator_,
+                description: description_,
+                location: location_,
+                amount: amount_,
+                blocked: true,
+                paid: false
+            });
 
         demands.push(demand);
 
@@ -80,7 +79,15 @@ contract Linkable {
     }
 
     // Emited when createDemand is called
-    event DemandCreated(string name, string creator, string description, string location, uint256 amount, bool blocked, bool paid);
+    event DemandCreated(
+        string name,
+        string creator,
+        string description,
+        string location,
+        uint256 amount,
+        bool blocked,
+        bool paid
+    );
 
     // Returns all DemandFormProject items
     function getAllDemand() public view returns (DemandFromProject[] memory) {
@@ -98,17 +105,17 @@ contract Linkable {
     // Emited when unBlock is called
     event unBlockedDemand(bool blocked);
 
-    // Transfer the funds to projectAddr 
+    // Transfer the funds to projectAddr
     function withdraw(uint256 i_) public onlyProject {
-         DemandFromProject storage demand = demands[i_];
-         require(!demand.blocked, "Linkable : permission denied");
-         require(!demand.paid, "Linkable : demand already paid");
-         projectAddr.transfer(demand.amount);
-         demand.paid = true;
+        DemandFromProject storage demand = demands[i_];
+        require(!demand.blocked, "Linkable : permission denied");
+        require(!demand.paid, "Linkable : demand already paid");
+        projectAddr.transfer(demand.amount);
+        demand.paid = true;
 
-         emit demandPaid(projectAddr, demand.amount);
-     }
+        emit demandPaid(projectAddr, demand.amount);
+    }
 
-     // Emited when withdraw is called
-     event demandPaid(address recipient_, uint256 amount_);
+    // Emited when withdraw is called
+    event demandPaid(address recipient_, uint256 amount_);
 }
